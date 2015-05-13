ee142dae465f85a4a34ff6b7326ed1caea43646b
George Miller <github@duffleman.co.uk>
2015-05-13 18:56:25 +0100

    Adjusting line spaces etc

--- database/migrations/2015_05_13_124215_create_int_logs_table.php
+++ database/migrations/2015_05_13_124215_create_int_logs_table.php
@@ -1,33 +1,31 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateIntLogsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('int_logs', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('int_logs', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('log');
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('int_logs');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('int_logs');
+    }
 }
