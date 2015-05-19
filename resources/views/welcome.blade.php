d27cee5bd24d4dd5b7a0b4258046ed2a42bf3111
George Miller <github@duffleman.co.uk>
2015-05-19 20:37:15 +0100

    Highres icon set <3 Shad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
 			<div class="row bottom40">
 				<div class="col-lg-12">
-					<img src="/img/logo.png" alt="Pushman Logo">
+					<img src="/img/logo.png" class="logo" alt="Pushman Logo">
 					<h1>Pushman</h1>
 					<p>The open source web socket event handler.</p>
 				</div>
@@ -16,21 +16,21 @@
 
 			<div class="row bottom40">
 				<div class="col-lg-4">
-					<img src="/img/simple.png" alt="Simple">
+					<img class="home-icon" src="/img/simple.png" alt="Simple">
 					<div class="text">
 						<h4>Simple</h4>
 						<p>Build new events in seconds</p>
 					</div>
 				</div>
 				<div class="col-lg-4">
-					<img src="/img/secure.png" alt="Secure">
+					<img class="home-icon" src="/img/secure.png" alt="Secure">
 					<div class="text">
 						<h4>Secure</h4>
 						<p>Open source at <a href="http://github.com/duffleman/pushman">GitHub</a></p>
 					</div>
 				</div>
 				<div class="col-lg-4">
-					<img src="/img/ready.png" alt="Ready">
+					<img class="home-icon" src="/img/ready.png" alt="Ready">
 					<div class="text">
 						<h4>Ready</h4>
 						<p>Feature rich</p>
