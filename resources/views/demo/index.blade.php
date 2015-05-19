8b8462756596dc43feb6862f925e69e9d9599505
George Miller <github@duffleman.co.uk>
2015-05-19 17:38:09 +0100

    Added some JS comments to the demo and updated the registration form so people can be tempted to build their own

--- resources/views/demo/index.blade.php
+++ resources/views/demo/index.blade.php
@@ -127,10 +127,24 @@
 <script src="http://autobahn.s3.amazonaws.com/js/autobahn.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 <script>
+/*
+ * Howdy! You should read this to see how the demo works.
+ * Don't be afraid to ping me on Twitter @Duffleman 
+ *
+ */
+
+// Holds the connections wer are listening to.
+// You don't need to do this, Pushman already handles clients trying to subscribe twice.
 var listening = [];
+// Holds our connection variable in the global scope.
 var conn;
+
 $(document).ready(function() {
+	// Check if we are connected every second.
+	// This only updates the Status: text.
 	setInterval(checkConnection, 1000);
+
+	// Connect the Pushman to begin.
 	connect();
 
 	// Handle the PUSH event form.
@@ -138,6 +152,7 @@ $(document).ready(function() {
 		event.preventDefault();
 		$('#response').html('');
 
+		// Grab the Form data.
 		var channel = $('#channel').val();
 		var event = $('#event').val();
 		var payload = $('#payload').val();
@@ -146,7 +161,9 @@ $(document).ready(function() {
 
 		// Emulates you pushing an event to us via a HTTP POST request.
 		$.post('/api/push', { channel: channel, event: event, payload: payload, private: private_key}, function(data) {
+			// It auto parses the JSON into an object. Lets turn it back into a string here.
 			var str = JSON.stringify(data, null, 2);
+			// Put it into the response div box.
 			$('#response').html(str);
 			// Don't worry about this bit, this just makes it look good on the UI.
 			hljs.highlightBlock(document.getElementById('response'));
@@ -156,25 +173,32 @@ $(document).ready(function() {
 	// Catch Connect and Disconnect buttons
 	$('#btnConnect').on('click', function(event) {
 		event.preventDefault();
+		// Connect to Pushman
 		connect();
 	});
 	$('#btnDisconnect').on('click', function(event) {
 		event.preventDefault();
+		// Disconnect from Pushman
 		disconnect();
 	});
 
 	// Handle the CATCH event form.
 	$('#listenForm').on('submit', function(event) {
 		event.preventDefault();
+		// Grab Form data
 		var event_name = $('#listen_event_name').val();
 
+		// Are they already listening to the event?
+		// If so, let's not bother Pushman, just tell them.
 		if(inArray(event_name, listening)) {
 			divLog('You are already listening to that event.');
 			return false;
 		} else {
 			listening.push(event_name);
+			// Assuming they are connected, lets subscribe to the event on the public channel.
 			if(checkConnection() == true) {
 				conn.subscribe(event_name, function(topic, data) {
+					// This is what happens when we catch the event!
 					divLog("Caught Event! "+event_name+" was caught with " + JSON.stringify(data));
 				});
 				divLog('Started listening to ' + event_name);
@@ -185,6 +209,9 @@ $(document).ready(function() {
 	});
 });
 
+/*
+ * This function checks to see if there is an element within an array for Javascript.
+ */
 function inArray(needle, haystack) {
 	var length = haystack.length;
 	for(var i = 0; i < length; i++) {
@@ -193,7 +220,13 @@ function inArray(needle, haystack) {
 	return false;
 }
 
+// This function handles connecting to Pushman via a WebSocket.
+// No point connecting if you're already connected so it'll always try to disconnect first.
 function connect() {
+	disconnect();
+
+	// This token changes every 60 minutes automatically.
+	// We use Serverside helpers so we never have to change the code.
 	conn = new ab.Session('ws://{{ PushPrep::getHost() }}:{{ PushPrep::getPort() }}?token={{ PushPrep::getDemo() }}',
 		function() {
 			divLog('Connected to Pushman via Web Socket.');
@@ -205,12 +238,19 @@ function connect() {
 	);
 }
 
+// This function handles disconnecting from Pushman.
+// Try and catch is needed incase the user tries to disconnect before the connection is orignally established.
 function disconnect()
 {
-	conn.close();
+	try {
+		conn.close();
+	} catch(err) {
+		// do nothing.
+	}
 	listening = [];
 }
 
+// This checks to see if we're currently connected to pushman.
 function checkConnection()
 {
 	var div = $('#connectionState');
@@ -230,6 +270,9 @@ function checkConnection()
 	}
 }
 
+
+// This is an easy function to push logs to both the console and the div "Output Log".
+// It auto appends a H:i:s timestamp at the begining.
 function divLog(text)
 {
 	var d = new Date();
@@ -246,6 +289,8 @@ var STR_PAD_LEFT = 1;
 var STR_PAD_RIGHT = 2;
 var STR_PAD_BOTH = 3;
 
+// This is used purely for the timestamp
+// It pads 0s for formatting for the Hour, Minute, and Second.
 function pad(str, len, pad, dir) {
 	if (typeof(len) == "undefined") { var len = 0; }
 	if (typeof(pad) == "undefined") { var pad = '0'; }
